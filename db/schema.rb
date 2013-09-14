131f8f46206426a2ad1f6fb1fe4d2e9365f20864
flastoe <ora@IreOne.local>
2013-09-14 04:47:11 -0500

    pg_set

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130913213131) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: true do |t|
     t.string   "name"
     t.integer  "subcategory_id"
